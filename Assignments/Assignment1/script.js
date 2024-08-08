9778432402c28d7a048577f0e6742f80171f1d4b
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-08-08 11:22:18 +0200

    Finished Assignment

--- Assignments/Assignment1/script.js
+++ Assignments/Assignment1/script.js
@@ -10,3 +10,72 @@ var pets = [
     { name: "Buddy", species: "dog", age: 10, adopted: true, adoptedDate: "2021-02-01", adoptionFee: 735 },
     { name: "Pebbles", species: "bird", age: 4, adopted: false, adoptedDate: "", adoptionFee: 505 },
 ];
+
+class PetHandler{
+    constructor(array){
+        this.pets = array;
+    }
+
+    findPetsInAgeRange(minAge,maxAge){
+        return this.pets.filter(pet => pet.age >= minAge && pet.age <= maxAge);
+    }
+
+    listAdoptedPetsByDate(){
+        return pets.filter(pet => pet.adopted).sort((a,b) => a.adoptedDate.localeCompare(b.adoptedDate));
+    }
+
+    listPets(...args){
+        const createPetItem = (pet) =>{
+            let petInfo = `${pet.name} | ${pet.species} | Age: ${pet.age}`;
+            if(pet.adopted){
+                petInfo += ` | Adopted: ${pet.adoptedDate}`;
+            }
+            return petInfo;
+        }
+
+        let myList;
+        if(args.length === 0){
+            myList = pets;
+        }
+        else if(args.length === 1 && Array.isArray(args[0])){
+            myList = args[0];
+        }
+        else{
+            myList = args;
+        }
+
+        return myList.map(createPetItem);
+    }
+
+    calculateUniqueAdoptionFee(...petNames){
+        const names = [...new Set(petNames)];
+        const petList = this.pets.filter(pet => names.includes(pet.name));
+        return petList.reduce((sum,pet) => sum + (pet.adoptionFee || 0), 0);
+    }
+}
+
+Array.prototype.findPetsInAgeRange = function(min,max){
+    return this.filter(pet => pet.age >= min && pet.age <= max);
+}
+
+Array.prototype.listPets = function(){
+    const createPetItem = (pet) =>{
+        let petInfo = `${pet.name} | ${pet.species} | Age: ${pet.age}`;
+        if(pet.adopted){
+            petInfo += ` | Adopted: ${pet.adoptedDate}`;
+        }
+        return petInfo;
+    };
+
+    return this.map(createPetItem);
+}
+
+Array.prototype.calculateUniqueAdoptionFee = function(...petNames){
+    const names = [...new Set(petNames)];
+    const petList = this.filter(pet => names.includes(pet.name));
+    return petList.reduce((sum,pet) => sum + (pet.adoptionFee || 0), 0);
+}
+
+Array.prototype.listAdoptedPetsByDate = function(){
+    return this.filter(pet => pet.adopted).sort((a,b) => a.adoptedDate.localeCompare(b.adoptedDate));
+}
