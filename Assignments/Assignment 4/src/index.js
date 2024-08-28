e3a5b16253f3c1d5548bd4f1383d68b630c70827
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-08-28 19:52:20 +0200

    Initial commit

---
+++ Assignments/Assignment 4/src/index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom";
+
+import {Post} from "./components/Post.js";
+
+ReactDOM.render(<Post title="SomeTitle" description="desc" author="Hayley" />, document.getElementById("root"));
