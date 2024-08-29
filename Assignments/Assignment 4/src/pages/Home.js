19a1fc72c098be895d5accbfc0948639477b1be1
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-08-29 02:13:11 +0200

    Added React Routing

--- Assignments/Assignment 4/src/pages/Home.js
+++ Assignments/Assignment 4/src/pages/Home.js
@@ -1,1 +1,14 @@
+import React from "react"
+import {Link} from 'react-router-dom';
 
+export class Home extends React.Component{
+    render(){
+        return(
+                <div>
+                    <h1>Hello Home Page!</h1>
+                    <Link to="/">Home</Link>
+                    <Link to="/posts">Posts</Link>
+                </div>
+            );
+    }
+}
