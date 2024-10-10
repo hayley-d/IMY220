8e6fdf202c514e635423717c0ccec7d71258e6e3
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-10-10 14:56:50 +0200

    Add files via upload

--- ClassCode/basic_react/src/components/HelloWorld.js
+++ ClassCode/basic_react/src/components/HelloWorld.js
@@ -1,1 +1,14 @@
+//Hayley Dodkins u21528790
+import React from 'react';
 
+export class HelloWorld extends React.Component{
+    
+    render(){
+        return(
+            <div>
+                <h5>Hayley Dodkins</h5>
+                <p>u21528790</p>
+            </div>
+        );
+    }
+}
