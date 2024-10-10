7535a48440d3fa371661e1014812c06c0005b4da
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-10-10 14:56:30 +0200

    add src files

---
+++ ClassCode/basic_react/src/App.js
@@ -0,0 +1,14 @@
+//Hayley Dodkins u21528790
+import React from "react"
+import {HelloWorld} from "./components/HelloWorld.js";
+
+export class App extends React.Component{
+     render(){
+        return(
+                <div>
+                    <HelloWorld />
+               </div>
+        );
+    }
+}
+
