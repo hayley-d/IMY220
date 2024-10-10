f607be46b7fa7e2fbb2b727c64e30be806d7a536
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-10-10 15:33:15 +0200

    add nav

--- ClassCode/react_router_old/src/components/Navbar.js
+++ ClassCode/react_router_old/src/components/Navbar.js
@@ -1,1 +1,15 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+import {Home} from '../pages/Home.js'
+import {Products} from '../pages/Products.js'
 
+export class Navbar extends React.Component{
+    render(){
+        return(
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/products">Products</Link>
+            </nav>
+        );
+    }
+}
