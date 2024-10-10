0744e45b958d600e552d6eb359ecd3e4f432cca0
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-10-10 15:34:47 +0200

    add pages

---
+++ ClassCode/react_router_old/src/pages/Product.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export class Product extends React.Component{
+    render(){
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
---
+++ ClassCode/react_router_old/src/pages/Products.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export class Products extends React.Component{
+    render(){
+        return(
+            <div>
+                <ul>
+                    <li><a href="/product/1">Product 1</a></li>
+                </ul>
+            </div>
+        );
+    }
+}
