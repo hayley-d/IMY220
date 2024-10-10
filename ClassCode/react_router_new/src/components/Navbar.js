cf76a9693ec57b280788bcb1e30c3171d3c17682
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-10-10 15:53:54 +0200

    Add files via upload

--- ClassCode/react_router_new/src/components/Navbar.js
+++ ClassCode/react_router_new/src/components/Navbar.js
@@ -1,1 +1,13 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
 
+export class Navbar extends React.Component{
+    render(){
+        return(
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/products">Products</Link>
+            </nav>
+        );
+    }
+}
