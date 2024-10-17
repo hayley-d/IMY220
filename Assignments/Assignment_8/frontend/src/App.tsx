171328d758144725c01a30e1da5c351c2856c276
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-10-17 12:50:56 +0200

    add entry components

--- Assignments/Assignment_8/frontend/src/App.tsx
+++ Assignments/Assignment_8/frontend/src/App.tsx
@@ -1,17 +1,12 @@
 import React from "react";
-import { Splash } from "./pages/Splash";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const router = createBrowserRouter([
-    { path: '/', element: <Splash /> },
-]);
+import { PokemonApp } from "./components/PokemonApp";
 
 export class App extends React.Component {
     render() {
         return (
-            <RouterProvider router={router}>
-                <Navbar/>
-            </RouterProvider>
+            <div>
+                <PokemonApp/>
+            </div>
         );
     }
 }
