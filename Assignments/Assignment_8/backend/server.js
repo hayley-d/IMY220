264847c47c8dd7bdfa16fbbc47e5f960f8b6bb8f
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-10-17 02:06:08 +0200

    add server

--- Assignments/Assignment_8/backend/server.js
+++ Assignments/Assignment_8/backend/server.js
@@ -1,1 +1,26 @@
+import express from "express";
+import * as path from "node:path";
+import cors from "cors";
+import bodyParser from "body-parser";
 
+const Routes = require(path.resolve("./backend/api.js"));
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+const PORT = 3001;
+app.use('/api', UserRoutes);
+
+app.use(express.static(path.resolve("./frontend/public")));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve("./frontend/public/index.html"));
+});
+
+const startServer = async () => {
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+};
+
+startServer();
