ceaa00aa2c32e6a4cc7141d8cce556c1024da6a0
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-10-17 12:49:23 +0200

    add updated configs

--- Assignments/Assignment_8/webpack.config.js
+++ Assignments/Assignment_8/webpack.config.js
@@ -2,15 +2,23 @@ const path = require("path");
 
 module.exports = {
     
-    entry: "./frontend/src/index.js",
+    entry: "./frontend/src/index.tsx",
     output: {
         path: path.resolve("frontend","public"),
         filename: "bundle.js",
         publicPath: '/'
     },
+    resolve: {
+        extensions:['.ts','.tsx','.js','.jsx']
+    },
     mode: "development",
     module: {
         rules: [
+                    {
+                        test: /\.tsx?$/,
+                        use: 'ts-loader',
+                        exclude: /node_modules/,
+                    },
                     {
                         test: /\.css$/,
                         use: ['style-loader', 'css-loader', 'postcss-loader'],
@@ -30,7 +38,8 @@ module.exports = {
         contentBase: path.resolve(__dirname, 'frontend', 'public'),
         hot: true,
         historyApiFallback: true,
-        port: 3000,
-        publicPath: '/'
+        port: 3001,
+        publicPath: '/',
+        hot:true
     }
 }
