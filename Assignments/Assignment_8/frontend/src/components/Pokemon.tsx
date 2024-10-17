65e4524680749baec10d4c18ae3608f22b3c9a4d
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-10-17 12:50:25 +0200

    add components

--- Assignments/Assignment_8/frontend/src/components/Pokemon.tsx
+++ Assignments/Assignment_8/frontend/src/components/Pokemon.tsx
@@ -3,28 +3,31 @@ import React from 'react';
 /**
  * Pokemon interface defines the structure of a Pokemon object type.
  */
-interface Pokemon {
+interface PokemonObj {
     id : number;
     name: string;
     height: number;
     weight: number;
-    type : string[];
+    types : { type: { name: string } }[];
 }
 
-export class Pokemon extends React.Component<Pokemon> {
+interface Props {
+    pokemon : PokemonObj;
+}
 
-    constructor(props:Pokemon) {
+export class Pokemon extends React.Component<Props> {
+
+    constructor(props:Props) {
         super(props);
     }
 
     render() {
-        const { id, name, height, weight, types } = this.props;
         return(
             <div> 
-                <h2>{name} #{id}</h2>
-                <p>Height: {height}</p>
-                <p>Weight: {weight}</p>
-                <p>Types: {types.map((type,index) => (<span key={index}>{type}{index < types.length -1 && ', '} </span>))}</p>
+                <h2>{this.props.pokemon.name} #{this.props.pokemon.id}</h2>
+                <p>Height: {this.props.pokemon.height}</p>
+                <p>Weight: {this.props.pokemon.weight}</p>
+                <p>Types: {this.props.pokemon.types.map((type : {type : {name:string}},index:number) => (<span key={index}>{type.type.name}{index < this.props.pokemon.types.length -1 && ', '} </span>))}</p>
             </div>
         );
     }
--- Assignments/Assignment_8/frontend/src/components/Search.tsx
+++ Assignments/Assignment_8/frontend/src/components/Search.tsx
@@ -1,34 +1,42 @@
 import React from 'react'
 
 interface SearchProps {
-    onSearch: (searchTerm: string) => void;
+    onSearch: (name: string) => void;
 }
 
-export class Search extends React.Component {
+interface IState {
+    error? : string;
+}
 
-    constructor(props) {
+export class Search extends React.Component<SearchProps,IState> {
+    private searchRef : React.RefObject<HTMLInputElement>;
+
+    constructor(props : SearchProps) {
         super(props);
-        this.state = {
-            search_term:""
-        }
-        this.handleInputChange = this.handleInputChange.bind(this);
+        this.state = {error:""};
+        this.searchRef = React.createRef(); 
         this.findPokemon = this.findPokemon.bind(this);
     }
 
-    findPokemon(e: React.FormEvent) {
+    findPokemon(e: React.FormEvent) : void {
         e.preventDefault();
-        this.props.onSearch(this.state.search_term);
-    }
-
-    handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
-        this.setState({search_term: e.target.value});
+        const search_term : string | undefined = this.searchRef.current?.value.trim();
+        if(!search_term) {
+            this.setState({error:"Search term is empty."});
+            return;
+        } else if(/\d/.test(search_term)) {
+            this.setState({error:"Name cannot contain numbers."});
+            return;
+        }
+        this.props.onSearch(search_term);
     }
 
     render() {
         return(
             <div>
                 <form>
-                    <input type="text" name="search" placeholder="Search Pokemon Name..." value={this.state.search_term} onChange={this.handleInputChange}/>
+                    <input type="text" name="search" placeholder="Search Pokemon Name..." ref={this.searchRef}/>
+                    {this.state.error && <p style={{ color: 'red' }}>{this.state.error}</p>}
                     <button onClick={this.findPokemon}>Search</button>
                 </form>
             </div>
