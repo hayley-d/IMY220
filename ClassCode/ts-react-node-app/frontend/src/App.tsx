08e0176455d1bf38f1493507d516a581a34114cf
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-10-22 07:46:51 +0200

    add other react components

---
+++ ClassCode/ts-react-node-app/frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { PokemonApp } from "./components/PokemonApp";
+
+export class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <PokemonApp/>
+            </div>
+        );
+    }
+}
+
