83641c34ba4facd5be16527e7e429f2090884a35
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-10-29 06:03:09 +0200

    final upload

--- Assignments/Assignment_9/server.js
+++ Assignments/Assignment_9/server.js
@@ -1,17 +1,25 @@
-import express from 'express';
-import fs from 'fs';
-const app = expresso();
+import express from "express";
+import fs from "fs";
+import path from 'path';
+const app = express();
 const port = 3000;
 
-app.get(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/recipes', (req, res) => {
-  fs.readFile('recipes.json', 'utf8', (err, data) => {
+app.get("/recipes", (req, res) => {
+  //const filePath = path.join(__dirname, "public", "recipes.json");
+  fs.readFile('./public/recipes.json', "utf8", (err, data) => {
     if (err) {
-      console.error(err);
+      res.status(500).send("Error occured while trying to open the file.");
       return;
     }
-    
+
+    try {
+      const recipes = JSON.parse(data);
+      res.status(200).json(recipes);
+    } catch (err) {
+      res.status(500).send("Error parsing file");
+    }
   });
 });
 
