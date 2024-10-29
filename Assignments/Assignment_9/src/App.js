e56e73bcf7a9f46e83a27d036f024c66dc48cc7b
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-10-29 06:03:40 +0200

    final upload

--- Assignments/Assignment_9/src/App.js
+++ Assignments/Assignment_9/src/App.js
@@ -1,59 +1,69 @@
 import React from "react";
 import { RecipeCard } from "./components/RecipeCard";
+import { RecipePage } from "./components/RecipePage";
 
+export class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            recipes: [],
+            currentRecipe: null,
+        };
+        this.componentDidMount = this.componentDidMount.bind(this);
+    }
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.props = {
-      recipes: "[]",
-      currentRecipe: null,
+    componentDidMount() {
+        fetch("/recipes")
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.json(); 
+            })
+            .then((data) => {
+                this.setState({recipes:data}); 
+            })
+            .catch((error) => {
+                console.error("Error fetching recipes:", error);
+            });
+    }
+
+    setCurrentRecipe = (id) => {
+        const recipe = this.state.recipes.find((recipe) => (recipe.id === id));
+        this.setState({ currentRecipe: recipe });
     };
-  }
 
-  componentDidUpdate() {
-    fetch("/recipes")
-      .then((response) => response.json())
-      .then((data) => {
-        this.setState({ recipes: data });
-      });
-  }
-
-  setCurrentRecipe = (id) => {
-    const recipe = this.state.recipes.find((recipe) => recipe.id = id);
-    this.setState({ currentRecipe: recipe });
-  };
-
-  render() {
-    return (
-      <div>
-        <h1>Tayla's Delicious Treats!</h1>
-        <div style={{ padding: "20px", display: "flex"}}>
-          <div style={{ flexBasis: "40%" }}>
-            {this.state.recipes &&
-              this.state.recipes.map((recipe) => {
-                return (
-                  <RecipeCard
-                    key={recipe.id}
-                    id={recipe.id}
-                    title={recipe.title}
-                    description={recipe.description}
-                    setCurrentRecipe={this.setCurrentRecipe}
-                  />
-                );
-              })}
-          </div>
-          <div>
-            {this.state.currentRecipe && (
-              <RecipePage
-                title={this.state.currentRecipe.title}
-                description={this.state.currentRecipe.description}
-                instructions={this.state.currentRecipe.instructions}
-              />
-            )}
-          </div>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1>Tayla's Delicious Treats!</h1>
+                <div style={{ padding: "20px", display: "flex" }}>
+                    <div style={{ flexBasis: "40%" }}>
+                        {this.state.recipes &&
+                            this.state.recipes.map((recipe,index) => {
+                                return (
+                                    <RecipeCard
+                                        key={index}
+                                        id={recipe.id}
+                                        title={recipe.title}
+                                        description={recipe.description}
+                                        setCurrentRecipe={this.setCurrentRecipe}
+                                    />
+                                );
+                            })}
+                    </div>
+                    <div>
+                        {this.state.currentRecipe && (
+                            <RecipePage
+                                id={this.state.currentRecipe.id}
+                                title={this.state.currentRecipe.title}
+                                description={this.state.currentRecipe.description}
+                                instructions={this.state.currentRecipe.instructions}
+                            />
+                        )}
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
